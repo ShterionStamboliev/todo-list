10b28bbbb7bd66207a5c316eeb491832a0536fb3
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-07-18 19:26:45 +0300

    .

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client"
+
+import React, { useState, useEffect } from 'react'
 import { Metadata } from "next"
 import styles from './styles.module.css'
 import { Button, TextField } from '@mui/material';
@@ -8,6 +10,20 @@ export const metadata: Metadata = {
 };
 
 const Login = () => {
+
+    const [userData, setUserData] = useState({
+        username: '',
+        password: ''
+    });
+
+
+    const handleUserData = (e: any) => {
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value
+        });
+    };
+
     return (
         <div className={styles['main__login__page']}>
             <div className={styles['main__login__page__header']}>
@@ -16,10 +32,29 @@ const Login = () => {
 
             <section className={styles['main__login__page__section']}>
                 <div className={styles['main__login__page__inputs']}>
-                    <TextField id="standard-basic" label="Username" variant="standard" className={styles['main__login__page__username']} />
-                    <TextField id="standard-basic" label="Password" variant="standard" type='password' className={styles['main__login__page__password']} />
+                    <TextField
+                        id="standard-basic"
+                        label="Username"
+                        variant="standard"
+                        name='username'
+                        value={userData.username}
+                        onChange={handleUserData}
+                        className={styles['main__login__page__username']} />
+                    <TextField
+                        id="standard-basic"
+                        label="Password"
+                        variant="standard"
+                        name='password'
+                        type='password'
+                        value={userData.password}
+                        onChange={handleUserData}
+                        className={styles['main__login__page__password']} />
                 </div>
-                <Button className={styles['main__login__page__sign_in']} variant="contained">Login</Button>
+                <Button
+                    className={styles['main__login__page__sign_in']}
+                    variant="contained">
+                    Login
+                </Button>
             </section>
         </div>
     )
