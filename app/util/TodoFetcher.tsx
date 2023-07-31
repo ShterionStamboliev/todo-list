3f0b5c6cfd1c8bfecd620058d63e06dfaa6ea339
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-07-31 19:15:30 +0300

    Added pagination

--- app/util/TodoFetcher.tsx
+++ app/util/TodoFetcher.tsx
@@ -11,16 +11,47 @@ import LoadingSpinner from '../components/Navigation/LoadingSpinner';
 import styles from '../my-todos/styles.module.css'
 import DeleteIcon from '@mui/icons-material/Delete';
 import { handleDeleteTodo } from '../helpers/todoDelete';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
 
 const TodoFetcher: React.FC<TodoProps> = () => {
 
     const [todoData, setTodoData] = useState<TodoProps[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(true);
-    const todoCollection = collection(db, 'todos');
+    const [todoPerPage, setTodoPerPage] = useState<number | any>(5);
+    const [currentPage, setCurrentPage] = useState<number | any>(1);
 
     useEffect(() => {
         getData();
     }, []);
+
+    const todoCollection = collection(db, 'todos');
+
+    const numberOfTodoPages = Math.ceil(todoData.length / todoPerPage);
+    const todoPages: number[] = [...Array(numberOfTodoPages + 1).keys()].slice(1);
+    const lastTodoIndex = currentPage * todoPerPage;
+    const firstTodoIndex = lastTodoIndex - todoPerPage;
+
+    const onPageTodos = todoData.slice(firstTodoIndex, lastTodoIndex);
+
+    const prevPageHandler = () => {
+        if (currentPage !== 1) {
+            setCurrentPage(currentPage - 1);
+        };
+    };
+
+    const nextPageHandler = () => {
+        if (currentPage !== numberOfTodoPages) {
+            setCurrentPage(currentPage + 1);
+        };
+    };
+
+    const handlePageChange = (event: SelectChangeEvent) => {
+        setTodoPerPage(event.target.value as string);
+    };
 
     const getData = async () => {
         try {
@@ -46,28 +77,53 @@ const TodoFetcher: React.FC<TodoProps> = () => {
     };
 
     return (
-        <div className={styles['todo__tasks__container']}>
-            {isLoading ?
-                <LoadingSpinner /> :
-                todoData.map((todo: TodoProps, i: number) => {
-                    return (
-                        <div className={styles['todo__card']} key={i}>
-                            <input
-                                className={styles['todo__radio__btn']}
-                                type="radio"
-                                id="radio-input"
-                            />
-                            {todo.title}
-                            <button
-                                onClick={() => handleDeleteTodo(todo.id)}
-                                className={styles['todo__delete_btn']}>
-                                <DeleteIcon />
-                            </button>
-                        </div>
-                    )
-                })}
-            <p className={styles['todo__card_paragraph']}>Drag and drop to reorder list</p>
-        </div>
+        <>
+            <Box sx={{ maxWidth: 170 }}>
+                <FormControl sx={{ minWidth: 170 }}>
+                    <InputLabel>Show</InputLabel>
+                    <Select
+                        label="Todo"
+                        onChange={handlePageChange}
+                    >
+                        <MenuItem value={5}>Five</MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
+            <div className={styles['todo__tasks__container']}>
+                {isLoading ?
+                    <LoadingSpinner /> :
+                    onPageTodos.map((todo: TodoProps, i: number) => (
+                        (
+                            <div className={styles['todo__card']} key={i}>
+                                <input
+                                    className={styles['todo__radio__btn']}
+                                    type="radio"
+                                    id="radio-input"
+                                />
+                                {todo.title}
+                                <button
+                                    onClick={() => handleDeleteTodo(todo.id)}
+                                    className={styles['todo__delete_btn']}>
+                                    <DeleteIcon />
+                                </button>
+                            </div>
+                        )
+                    ))}
+                {/* <p className={styles['todo__card_paragraph']}>Drag and drop to reorder list</p> */}
+            </div>
+
+            <div className={styles['todo__paginator']}>
+                <button onClick={prevPageHandler} className={styles['page__btn_changer']}>Prev</button>
+                {todoPages.map((page => (
+                    <button
+                        key={page}
+                        onClick={() => setCurrentPage(page)}
+                        className={currentPage === page ? styles['todo__page_btn'] : styles['todo__cursor_page']}>{page}</button>
+                )))}
+                <button onClick={nextPageHandler} className={styles['page__btn_changer']}>Next</button>
+            </div>
+        </>
     )
 }
 
