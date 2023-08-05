04463f89ef4c47960935265263f286f55c7015ac
Shterion Stamboliev <shterion.stamboliev@gmail.com>
2023-08-05 22:46:08 +0300

    Changed the loading spinner animation

--- app/components/Navigation/LoadingSpinner.tsx
+++ app/components/Navigation/LoadingSpinner.tsx
@@ -4,7 +4,9 @@ import styles from './styles.module.css'
 const LoadingSpinner = () => {
     return (
         <div className={styles['loading__spinner__container']}>
-            <div className={styles['loading__spinner']}></div>
+            <div className={styles['loader']}>
+
+            </div>
         </div>
     );
 };
